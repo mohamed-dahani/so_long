674adb34573a60c590493242b7d75f2591cfa84c
Mohamed Dahani <mdahani@e2r9p11.1337.ma>
2025-01-30 13:27:26 +0100

    start 01

---
+++ check_map.c
@@ -0,0 +1,18 @@
+
+int check_size()
+{
+    //check size of resolution
+}
+
+int check_regtangel(char *str)
+{
+    //check nafes size 
+}
+int check_valid_charcter()
+{
+    // P C E 1 0
+}
+int flood_fill()
+{
+    
+}
