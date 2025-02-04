5dc7c5d4fdb06c5c64cdf13507e4f5bd82b51c37
Mohamed Dahani <mdahani@e2r8p11.1337.ma>
2025-02-04 22:52:49 +0100

    finally run the window

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 13:08:34 by mdahani           #+#    #+#             */
-/*   Updated: 2025/02/04 17:02:52 by mdahani          ###   ########.fr       */
+/*   Updated: 2025/02/04 22:48:47 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,8 @@ int	main(int ac, char **av)
 	if (!check_map(av[1], &map))
 		return (custom_error("Error\nInvalid map !\n"), 1);
 	if (!run_window(&map, &window))
+	{
+		mlx_destroy_window(window.mlx, window.window);
 		return (custom_error("Error\nWindow is not running !\n"), 1);
+	}
 }
