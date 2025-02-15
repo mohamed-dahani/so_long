42e82678e88a6835ae574fa39fb74319285ab286
Mohamed Dahani <mdahani@c4r8p8.1337.ma>
2025-02-15 17:53:32 +0100

    fix bug

--- helper-functions/animate_enemy_bonus.c
+++ helper-functions/animate_enemy_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   animate_enemy.c                                    :+:      :+:    :+:   */
+/*   animate_enemy_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 16:23:32 by mdahani           #+#    #+#             */
-/*   Updated: 2025/02/13 22:51:17 by mdahani          ###   ########.fr       */
+/*   Updated: 2025/02/15 17:36:16 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,16 @@ static int	game_over(t_enemy *enemy, t_map *map, int new_y)
 
 static void	check_enemy_moves(t_map *map, int new_y, t_enemy *enemy)
 {
-	if (new_y < 0 || new_y >= map->rows || map->map[new_y][enemy->x] == '1')
+	if (new_y < 0 || new_y >= map->rows || map->map[new_y][enemy->x] == '1'
+		|| map->map[new_y][enemy->x] == 'N' || map->map[new_y][enemy->x] == 'C'
+		|| map->map[new_y][enemy->x] == 'E')
 	{
 		enemy->dir *= -1;
 		new_y = enemy->y + enemy->dir;
 	}
-	if (new_y >= 0 && new_y < map->rows && map->map[new_y][enemy->x] != '1')
+	if (new_y >= 0 && new_y < map->rows && map->map[new_y][enemy->x] != '1'
+		&& map->map[new_y][enemy->x] != 'N' && map->map[new_y][enemy->x] != 'C'
+		&& map->map[new_y][enemy->x] != 'E')
 	{
 		map->map[enemy->y][enemy->x] = '0';
 		enemy->y = new_y;
